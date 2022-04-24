c5fd105aa3be2e1bd69966b8b6b06c76f4f2357f
Edward <mralis@dpkgsoft.com>
2022-04-24 15:24:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
